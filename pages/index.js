1779bd1adc8dd8b8ccbea36df5320b2494100fce
Vipul Kumar <139807589+VipulBaliyan1912@users.noreply.github.com>
2024-01-24 12:53:11 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
-  
+  const [num,setNum]=useState(5);
      
   return (
     <div>
@@ -11,11 +11,11 @@ export default function Home() {
                 <h2>Factorial Calculation</h2>
                 <input 
                     type="number" 
-                    
-                   } 
+                    value={num}
+                    onChange={(e)=>setNum(e.target.value)}
                 />
        
-                <FactorialComponent  />
+                <FactorialComponent num={num} />
             </section>
     </div>
 );
